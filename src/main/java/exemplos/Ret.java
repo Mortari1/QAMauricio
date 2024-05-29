fa5ed3575384d5e8e652d457ec98f1c97569c6f1
HENRIQUE SILVA MORTARI <henrique.4105@aluno.pr.senac.br>
2024-05-28 21:16:31 -0300

    Terceiro: adicionando o teste

--- src/main/java/exemplos/Ret.java
+++ src/main/java/exemplos/Ret.java
@@ -1,5 +1,9 @@
 package exemplos;
 
 public class Ret {
-
+	
+	public double adicao(double n1, double n2) {
+		
+		return n1 + n2;
+	}
 }
--- src/main/java/exemplos/RetTest.java
+++ src/main/java/exemplos/RetTest.java
@@ -1,5 +1,17 @@
 package exemplos;
 
-public class RetTest {
+import org.junit.jupiter.api.Test;
 
+import junit.framework.*;
+public class RetTest {
+	
+	@Test
+	public void testaAdicao() {
+		double result;
+		Ret conta = new Ret();
+		
+		result = conta.adicao(10.50, 10.50);
+		
+		Assert.assertEquals(22, result, 0);
+	}
 }
